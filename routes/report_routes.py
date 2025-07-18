051f72939e405fb128e324ab9d1aed4d95b93e07
ishwari-0522 <ishwarichopade18@gmail.com>
2025-07-18 14:56:37 +0530

    delete

--- routes/report_routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, request, jsonify
-
-report_bp = Blueprint("report_bp", __name__)
-
-@report_bp.route("/report", methods=["POST"])
-def report():
-    data = request.json
-    print("Received report:", data)
-    return jsonify({"message": "Report received"}), 201
