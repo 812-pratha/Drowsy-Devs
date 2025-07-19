82b11ba82ea2c53ad7b26fab8a83ff02aa545523
812-pratha <prathaother@gmail.com>
2025-07-19 05:42:32 +0530

    Audit

--- next-app/app/map/page.js
+++ next-app/app/map/page.js
@@ -4,7 +4,7 @@ export default function MapPage() {
     return (
         <div className="h-screen">
             <iframe
-                src="http://localhost:3001/map"
+                src="http://localhost:3000/map"
                 width="100%"
                 height="100%"
                 title="Heatmap"
